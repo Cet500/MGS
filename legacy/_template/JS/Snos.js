db05f03d5536392f872eac4b34ebe2ea4f25d43a
= <Cet@si.ru>
2020-09-03 17:34:42 +1000

    delete old legacy template

--- legacy/_template/JS/Snos.js
+++
@@ -1,4 +0,0 @@
-window.onload = function(){
-	$("header").html('<a href="../Main-content.html" class="logo">BushCherry</a> <div class="Blokdliknopok"> <a href="#" class="obl" onclick="setLightTheme()">Light</a> <a href="#" class="obl" onclick="setDarkTheme()">Dark</a> </div>');
-	$("nav").html('<h3>Меню</h3><ul><li><a href="">Аркада</a></li><li><a href="">Action</a></li><li><a href="">Шутеры</a></li><li><a href="">Приключения</a></li><li><a href="">Стратегии</a></li><li><a href="">RPG</a></li><li><a href="">Гонки</a></li><li><a href="">Квесты</a></li><li><a href="">Симуляторы</a></li><li><a href="">Спортивные игры</a></li><li><a href="">Хоррор игры</a></li></ul>')
-}
