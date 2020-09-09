5a3c09a389c46077e5069ae8d0d2ffa83b67e4d1
Cet <Cet@si.ru>
2020-09-09 23:21:28 +1000

    relocate games system requires

--- code/php-mgs/php/DB/relocate.php
+++ code/php-mgs/php/DB/relocate.php
@@ -8,41 +8,86 @@
 		margin: 0px;
 		font-family: Consolas;
 	}
+	table{
+		border-collapse: collapse;
+		font-family: Consolas;
+		font-size: 0.8em;
+	}
+	table th{
+		background-color: #90ee90;
+		border: 1px solid black;
+		padding: 3px 7px;
+	}
+	table tr:nth-child(2n){
+		background-color: rgba(0,0,0,0.2);
+	}
+	table td{
+		border: 1px solid gray;
+		padding: 3px 7px;
+	}
 </style>
 
 <?php
-	/*
-
+	
+/*
 	require '../db.php';
 
-	$i = 0;
+	$a = 0;
+	$b = 0;
+	$array = [];
 
 	$games = R::getAll('SELECT * FROM `parserdata`');
 
 	foreach ( $games as $game ) {
-		$i++;
-		$array = [];
+		$a++;
+		$b = 0;
+		$array[$a] = [];
 
-		$str = $game['features'];
-
+		$str = $game['system_require'];
 		$str = substr($str, 4, -5);
+		$array_strs = explode("</li><li>", $str);
 
-		$array = explode("</li><li>", $str);
-
-		print('<br>============================ '.$game['id'].' ============================<br><br>');
-
-		foreach ($array as $feature) {
-
-			if ($array[0] != "") {
-				$data = R::dispense('gamefeatures');
-					$data->id_game = $game['id'];
-					$data->feature = $feature;
-				R::store($data);	
-			}
-			
-			print($feature.'<br>');
-		};
+		foreach ($array_strs as $array_str) {
+			$b++;
+			$array[$a][0]  = $game['id'];
+			$array[$a][$b] = multi_explode(array(":", ": ", ":  ", ":	", "	"), $array_str);	
+		}
 
 	}
-	
-	*/
+
+	print('<table style="width: 1000px;">');
+
+	foreach ($array as $arr) {
+		for ($i = 1; $i < count($arr); $i++) { 
+			print('<tr><td>'.$arr[0].'</td><td>'.$arr[$i][0].'</td><td>'.$arr[$i][1].'</td></tr>');
+
+			$sysreqdata = R::dispense('parsersysreq');
+				$sysreqdata->id_game        = $arr[0];
+				$sysreqdata->sys_part       = $arr[$i][0];
+				$sysreqdata->sys_require    = $arr[$i][1];
+			R::store($sysreqdata);
+		}
+	}
+
+	// print('</table>');
+
+	// print_r(array_unique($paramets));
+
+	// $paramets = [];
+	// $i = 0;
+
+	// foreach ($array as $arr) {
+	// 	foreach ($arr as $a) {
+	// 		$i++;
+	// 		$paramets[$i] = $a[0];
+	// 	}
+	// }
+
+	// print_r(array_unique($paramets));
+
+
+	function multi_explode ($delimiters,$string) {
+		$ready = str_replace($delimiters, $delimiters[0], $string);
+		$launch = explode($delimiters[0], $ready);
+		return  $launch;
+	}
